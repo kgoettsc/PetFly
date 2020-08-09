daf9e2eab671b763a44d4eaba6f6919b4fe3227c
Karl Goettsche <karl.goettsche@gmail.com>
2020-08-09 00:23:29 -0400

    fix match with type columns

--- app/services/match_service.rb
+++ app/services/match_service.rb
@@ -2,8 +2,8 @@ class MatchService
   class << self
     def flight_matches(flight)
       rescues = Rescue.
-                  joins('LEFT JOIN rescue_airports as departing_rescue_airports on rescues.id=departing_rescue_airports.rescue_id').
-                  joins('LEFT JOIN rescue_airports as arriving_rescue_airports on rescues.id=arriving_rescue_airports.rescue_id').
+                  joins("LEFT JOIN rescue_airports as departing_rescue_airports on rescues.id=departing_rescue_airports.rescue_id AND departing_rescue_airports.type = 'DepartingRescueAirport'").
+                  joins("LEFT JOIN rescue_airports as arriving_rescue_airports on rescues.id=arriving_rescue_airports.rescue_id AND arriving_rescue_airports.type = 'ArrivingRescueAirport'").
                   where(status: 'active').
                   where("available_from <= ?", flight.departing_at).
                   where(departing_rescue_airports: {airport_id: flight.departing_airport_id}).
@@ -15,8 +15,9 @@ class MatchService
 
     def rescue_matches(_rescue)
       flights = Flight.active.
-                  joins('LEFT JOIN rescue_airports as departing_rescue_airports on flights.departing_airport_id = departing_rescue_airports.airport_id').
-                  joins('LEFT JOIN rescue_airports as arriving_rescue_airports on flights.arriving_airport_id = arriving_rescue_airports.airport_id').
+                  joins("LEFT JOIN rescue_airports as departing_rescue_airports on flights.departing_airport_id = departing_rescue_airports.airport_id AND departing_rescue_airports.type = 'DepartingRescueAirport'").
+                  joins("LEFT JOIN rescue_airports as arriving_rescue_airports on flights.arriving_airport_id = arriving_rescue_airports.airport_id AND arriving_rescue_airports.type = 'ArrivingRescueAirport'").
+                  where("departing_at >= ?", _rescue.available_from).
                   where(departing_rescue_airports: {rescue_id: _rescue.id}).
                   where(arriving_rescue_airports: {rescue_id: _rescue.id}).
                   uniq
